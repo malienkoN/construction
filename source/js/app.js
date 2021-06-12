fb9e92ade91bb58019bdb81adcb2adc92a1398b8
malienkoN <nekmalienko2001@gmail.com>
2021-06-12 21:16:59 +0300

    gg first day

--- source/js/app.js
+++ source/js/app.js
@@ -1,1 +1,33 @@
-document.querySelector('h1').style.color = 'blue';
+window.addEventListener('load', function () {
+    const swiper = new Swiper('.mySwiper', {
+        slidesPerView: 3.5,
+        spaceBetween: 20,
+        centeredSlides: true,
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        breakpoints: {
+            // when window width is >= 320px
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 20
+            },
+            // when window width is >= 480px
+            768: {
+                slidesPerView: 2.5,
+                spaceBetween: 20
+            },
+            // when window width is >= 640px
+            1280: {
+                slidesPerView: 3.5,
+                spaceBetween: 20
+            },
+            1600: {
+                slidesPerView: 4.5,
+                spaceBetween: 20
+            }
+        },
+    });
+});
