e4da3159016952b27eede6b7e2c6d3b2dbca3f94
malienkoN <nekmalienko2001@gmail.com>
2021-06-14 14:29:06 +0300

    swiper

--- source/js/app.js
+++ source/js/app.js
@@ -1,9 +1,11 @@
 window.addEventListener('load', function () {
     const swiper = new Swiper('.mySwiper', {
-        slidesPerView: 3.5,
-        spaceBetween: 20,
         centeredSlides: true,
         loop: true,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
         pagination: {
             el: '.swiper-pagination',
             clickable: true,
@@ -16,22 +18,22 @@ window.addEventListener('load', function () {
             // when window width is >= 320px
             320: {
                 slidesPerView: 1,
-                spaceBetween: 20
+                spaceBetween: 40,
             },
             // when window width is >= 480px
             768: {
                 slidesPerView: 2.5,
-                spaceBetween: 20
+                spaceBetween: 20,
             },
             // when window width is >= 640px
             1280: {
                 slidesPerView: 3.5,
-                spaceBetween: 20
+                spaceBetween: 20,
             },
             1600: {
                 slidesPerView: 4.5,
-                spaceBetween: 20
-            }
+                spaceBetween: 20,
+            },
         },
     });
 });
